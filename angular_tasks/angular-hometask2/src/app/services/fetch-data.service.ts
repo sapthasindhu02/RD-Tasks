06a372d13f8470242f0ee0e059e582acc21d9129
Saptha Sindhu <sapthasindhu.v11@gmail.com>
2022-01-13 16:23:57 +0530

    completed angular tasks

--- angular_tasks/angular-hometask2/src/app/services/fetch-data.service.ts
+++ angular_tasks/angular-hometask2/src/app/services/fetch-data.service.ts
@@ -1,40 +1,31 @@
-import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, Observable, retry, throwError } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FetchDataService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor(private httpClient:HttpClient) { }
+  private REST_API_SERVER = 'http://localhost:3000/users';
 
-  private REST_API_SERVER="http://localhost:3000/users";
+  current_page: string;
+  userArr: Array<any>;
+  update: boolean = true;
+  createTrigger: boolean = false;
 
-  current_page:string;
-  userArr:Array<any>;
-  update:boolean=true;
-  createTrigger:boolean=false;
-
-  getUsers():Observable<any>{
-    return this.httpClient.get(this.REST_API_SERVER,{ 'responseType':'json'}).pipe(retry(3),catchError(this.handleError));
+  getUsers(): Observable<any> {
+    return this.httpClient.get(this.REST_API_SERVER, { responseType: 'json' });
   }
-  createUser(userObj:any){
-    return this.httpClient.post(`${this.REST_API_SERVER}`,userObj);
+  createUser(userObj: any) {
+    return this.httpClient.post(`${this.REST_API_SERVER}`, userObj);
   }
-  updateUserStatus(userObj:any,id:any){
-    return this.httpClient.put(`${this.REST_API_SERVER}/${id}`,userObj);
+  updateUserStatus(userObj: any, id: any) {
+    return this.httpClient.put(`${this.REST_API_SERVER}/${id}`, userObj);
   }
-  handleError(error:HttpErrorResponse){
-    let errmsg='Unknown';
-    if(error.error instanceof ErrorEvent){
-      errmsg=`Error:${error.error.message}`;
-    }
-    else{
-      errmsg=`Error code:${error.status}\n Message:${error.message}`;
-    }
-    window.alert(errmsg);
-    return throwError(errmsg);
-  }
-  
-  }
+}
